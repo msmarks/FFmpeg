10a7ccaceb56cd4ed1275a155900fb6087fe9f56
Cosmin Gorgovan <cosmin@linux-geek.org>
2020-08-20 21:05:23 +0100

    libavcodec/mmaldec: enable MJPEG decoding

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -184,6 +184,7 @@ extern AVCodec ff_mdec_decoder;
 extern AVCodec ff_mimic_decoder;
 extern AVCodec ff_mjpeg_encoder;
 extern AVCodec ff_mjpeg_decoder;
+extern AVCodec ff_mjpeg_mmal_decoder;
 extern AVCodec ff_mjpegb_decoder;
 extern AVCodec ff_mmvideo_decoder;
 extern AVCodec ff_motionpixels_decoder;
